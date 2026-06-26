0298e580e3c72c56e1166a45d62ea9e870bd8f72
RemusYamalius <idrissi.maj@gmail.com>
2026-06-26 05:24:37 +0100

    Delete src/routes/sitemap[.]xml.tsx

--- src/routes/sitemap[.]xml.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/sitemap.xml")({
-  component: () => null,
-  loader: () => {
-    return null;
-  },
-});
